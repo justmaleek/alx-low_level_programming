bc3ee012cc86cbd7cca756e8ca7b95762614f498
justmaleek <agbajesisuadepoju14@ju14@gmail.com>
2022-03-16 12:02:40 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * times_table - prints the times table of an integer starting with zero
- *
- * Return: Always 0.
- */
+* times_table - prints the times table of an integer starting with zero
+*
+* Return: Always 0.
+*/
 
 void times_table(void)
 {
